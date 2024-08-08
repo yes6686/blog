a5d4eeeb23c6ef57c28341e3b30bffd984bd61c5
안예찬 <yes490411@gmail.com>
2024-08-09 00:14:49 +0900

    버튼 기능 개발

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+/* eslint-disable */
+
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  
+  let post = '강남 우동 맛집';
+  const [title, setTitle] = useState(['남자 코트 추천','강남 우동 맛집','파이썬독학'])
+  let [goodCnt, setGoodCnt] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4 style={{color:'red', fontSize : '18px'}}>blog</h4>
+      </div>
+      <div className="list">
+        <h4>{title[0]} <span onClick={()=>{setGoodCnt(goodCnt+1)}}>👍</span> {goodCnt} </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      
+      <div className="list">
+        <h4>{title[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <h4>{post}</h4>
     </div>
   );
 }
